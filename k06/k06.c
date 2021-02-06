7080b517cc861c7937a29249b2dc14a41af3b841
yamaguchi akira <bb35319052@ms.nagasaki-u.ac.jp>
2021-02-06 22:20:56 +0900

    提出します

--- k06/k06.c
+++ k06/k06.c
@@ -72,7 +72,35 @@ void DynamicProgLimited(Menu arrayItem[], int items, int nap_size)
     int history[items+1][nap_size + 1];     //  履歴を保存するテーブル(選択したメニューを探すときに使用)
 
     //　ここを実装する
+    int i, j, k, n;
 
+    for(i=0; i<items+1; i++){
+        for(j=0; j<nap_size+1; j++){
+            nap_value[i][j] = 0;
+        }
+    }
+
+    for(i=1; i<=items; i++){
+        for(k=1; k < arrayItem[i-1].price; k++){
+            nap_value[i][k] = nap_value[i-1][k];
+            history[i][k] = k;
+        }
+        for(j=k+1; j<=nap_size; j++){
+            n = nap_value[i-1][j-arrayItem[i-1].price] + arrayItem[i-1].calorie;
+            if(n > nap_value[i-1][j]){
+                nap_value[i][j] = n;
+                history[i][j] = j;            
+            }
+            else
+            {
+                nap_value[i][j] = nap_value[i-1][j];
+                history[i][j] = j;
+            }
+        }
+    }
+
+    printf("MAXcalorie = %d\n", nap_value[items][nap_size-1]);
+    return;
 
 }
 
